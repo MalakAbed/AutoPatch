99f54ebe2c54fe01005ab22dcf734da8186c45c7
MalakAbed <malakabed.1999@gmail.com>
2025-12-21 18:46:18 +0200

    Auto-Patch: fix security issues in test.js

--- test.js
+++ test.js
@@ -1,5 +1,8 @@
 console.log("Hello AutoPatch! v2");
-const password = "123456";
-const apiKey = "HARDCODED_SECRET_KEY";
-console.log("Testing insecure code!", password, apiKey);
+const password = process.env.PASSWORD;
+const apiKey = process.env.API_KEY;
+if (!password || !apiKey) {
+  console.error("Missing sensitive information.");
+  process.exit(1);
+}
 console.log("Hello AutoPatch! v3");
