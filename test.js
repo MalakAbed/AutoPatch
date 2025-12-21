c40f49b2a758fc59ce575574baf3961b38847ae5
MalakAbed <malakabed.1999@gmail.com>
2025-12-21 20:43:05 +0200

    AutoPatch: fix security issues (a19ac2a)

--- test.js
+++ test.js
@@ -1,2 +1,8 @@
-const password = "123456";
-console.log("Testing insecure code!", password);
+const password = process.env.PASSWORD;
+if (!password) {
+    console.error("Error: No password provided.");
+    process.exit(1);
+} else {
+    // Removed sensitive log message for security reasons.
+    // Secure code is being tested.
+}
