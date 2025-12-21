4a96a8fe6d6bb85a34f0ae0a2ff9401304bdb41b
MalakAbed <malakabed.1999@gmail.com>
2025-12-21 20:38:06 +0200

    AutoPatch: fix security issues (9f35b04)

--- test.js
+++ test.js
@@ -1,4 +1,8 @@
 console.log("Hello AutoPatch!");
 console.log("Hello AutoPatch! 2");
-const password = "123456";
-console.log("Testing insecure code!", password);
+const password = process.env.PASSWORD;
+if (!password || password === "default_password") {
+    console.error("Error: Password is not set or is using the default value.");
+    process.exit(1);
+}
+console.log("Testing secure code!");
