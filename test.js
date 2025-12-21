632131007954c6f37f54baf652ac4f7341247db9
MalakAbed <malakabed.1999@gmail.com>
2025-12-21 18:53:21 +0200

    Auto-Patch: fix security issues in test.js

--- test.js
+++ test.js
@@ -1,3 +1,14 @@
 console.log("Hello AutoPatch! v2");
-const password = "123456";
-console.log("Testing insecure code!", password);
+const password = process.env.PASSWORD;
+if (!password) {
+    console.error("Password not set in environment variables.");
+    process.exit(1);
+}
+// Removed insecure logging of the password
+// console.log("Testing insecure code!", password);
+
+// Validate password format if applicable
+if (password.length < 8) {
+    console.error("Password must be at least 8 characters long.");
+    process.exit(1);
+}
