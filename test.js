9b81f655696127db1ec785e916d85534942e4c64
MalakAbed <malakabed.1999@gmail.com>
2025-12-22 22:31:31 +0200

    Auto-Patch: fix security issues in test.js

--- test.js
+++ test.js
@@ -1,4 +1,9 @@
-const password = "123456";
-console.log("Testing insecure code!", password); 
-const apikey = "123@456";
-console.log("Testing insecure code!", apikey);
+const password = process.env.PASSWORD;
+const apikey = process.env.APIKEY;
+
+if (!password || !apikey) {
+  console.error('Missing environment variables for password or API key.');
+  process.exit(1);
+}
+
+console.log("Testing secure code!");
