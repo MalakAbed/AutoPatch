d5bd531df5defb8b6e338a3a474bd27f9e2668d0
MALAK E M ABED <memabed@students.iugaza.edu.ps>
2026-01-19 10:54:12 +0200

    Auto-Patch: fix security issues in test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-const password = "1234567";
-console.log("Testing insecure code!", password); 
-const apikey = "123@456";
+const password = process.env.PASSWORD;
+const apikey = process.env.APIKEY;
+console.log("Testing insecure code!", password);
 console.log("Testing insecure code!", apikey);
 console.log("Hello Malak!");
